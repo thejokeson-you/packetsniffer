7464f665b057c5de556fe525bd09affb5002645c
thejokeson-you <milansaleh2004@gmail.com>
2024-07-17 17:47:07 +0100

    Add ICMP & TCP functions

--- main.py
+++ main.py
@@ -66,7 +66,31 @@ def formatted_ipv4(address):
     return '.'.join(map(str, address))
 
 
+def icmp_packet(data):
+    """
+    Unpack ICMP data
+    :param data:
+    :return:
+    """
+    icmp_type, code, checksum = struct.unpack('! B B H', data[:4])
+    return icmp_type, code. checksum, data[4:]
 
+
+def tcp_segment(data):
+    """
+    Unpack TCP segment
+    :param data:
+    :return:
+    """
+    (src_port, dest_port, sequence, acknowledgement, offset_reserved_flags) = struct.unpack('! H H L L H', data[:14])
+    offset = (offset_reserved_flags >> 12) * 4
+    flag_urg = (offset_reserved_flags & 32) >> 5
+    flag_ack = (offset_reserved_flags & 16) >> 4
+    flag_psh = (offset_reserved_flags & 8) >> 3
+    flag_rst = (offset_reserved_flags & 4) >> 2
+    flag_syn = (offset_reserved_flags & 2) >> 1
+    flag_fin = offset_reserved_flags & 1
+    return src_port, dest_port, sequence, acknowledgement, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin, data[offset:]
 
 
 
